bd53cbe8c3e1ac67ea1aa7601c718e55f32d5af6
Unknown <joeylubke@hotmail.nl>
2019-04-17 13:47:38 +0200

    Start on checkEverything method

--- PAD/src/assets/js/registratie.js
+++ PAD/src/assets/js/registratie.js
@@ -1,12 +1,23 @@
 $(document).ready(function () {
     $('#signup-form').submit(function(e) {
+        e.preventDefault();
         var enteredFullname = document.getElementById('registerFullname').value;
         var enteredEmail = document.getElementById('registerEmail').value;
         var enteredUsername = document.getElementById('registerUsername').value;
         var enteredNumberplate = document.getElementById('registerNumberplate').value;
         var enteredPassword = document.getElementById('registerPassword').value;
+        checkEverything();
 
-        
-        e.preventDefault();
-    })
+        function checkEverything() {
+            if(enteredFullname.length < 5 || enteredFullname.length > 16) {
+                alert("Username moet tussen de 5 en de 15 tekens zijn.");
+            }
+            if(!(enteredEmail.includes("@") && enteredEmail.includes("."))) {
+                alert("Goodjob je kan niks");
+            } else {
+                //indexof gebruiken voor locatie van @ en . bij toekomstige check
+                alert("Goodjob bro")
+            }
+        }
+    });
 });
