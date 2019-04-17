311a86027a94ec85dc3e556f0201e6a11bad6820
Unknown <joeylubke@hotmail.nl>
2019-04-17 13:11:32 +0200

    Catch values

--- PAD/src/assets/js/registratie.js
+++ PAD/src/assets/js/registratie.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
+    $('#signup-form').submit(function(e) {
+        var enteredFullname = document.getElementById('registerFullname').value;
+        var enteredEmail = document.getElementById('registerEmail').value;
+        var enteredUsername = document.getElementById('registerUsername').value;
+        var enteredNumberplate = document.getElementById('registerNumberplate').value;
+        var enteredPassword = document.getElementById('registerPassword').value;
 
+        
+        e.preventDefault();
+    })
 });
