52453d3a608af3a6a622ed2043ae53d82d75be40
BugBot6 <33983569+BugBot6@users.noreply.github.com>
2019-05-08 21:10:13 +0200

    Update api.php
    proper indentation.

--- ParkeerplaatsAPI/php/api.php
+++ ParkeerplaatsAPI/php/api.php
@@ -10,10 +10,9 @@
 		$availableSpots = 0;
 		$api = "https://amsterdam-maps.bma-collective.com/embed/elektrisch/laden/chargingstations.bb.json.php";
 		$parkingspots = array("EVB-P1629220","EVB-P1629218","EVB-P1811500","EVB-P1629219","EVB-P1811123");
-    	$contents = file_get_contents($api);
-    	$chargingPoints = json_decode($contents, true);
-    	//print_r($chargingPoints);
-    	foreach($chargingPoints as $chargingPoint) {
+    		$contents = file_get_contents($api);
+    		$chargingPoints = json_decode($contents, true);
+    		foreach($chargingPoints as $chargingPoint) {
  			if(in_array($chargingPoint['CSExternalID'], $parkingspots)){
  				$ports = $chargingPoint['ChargingPoints']['ChargingPoint'];
  				foreach ($ports as $port) {
