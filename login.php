7f90e34d7060a6aada2f1b6dcfdb783c9d270cd9
bugbot6 <bugbot6@gmail.com>
2019-05-13 16:45:23 +0200

    FInished login.php

--- login.php
+++ login.php
@@ -15,8 +15,25 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$stmt = $conn->prepare("SELECT COUNT(*) FROM klant WHERE Gebruikersnaam = ?");
+$stmt = $conn->prepare("SELECT Salt FROM klant WHERE Gebruikersnaam = ?");
 $stmt->bind_param("s", $_POST['username']);
+$stmt->execute();
+
+$result = $stmt->get_result();
+$passwordSalt = $result->fetch_row()[0];
+$stmt->close();
+
+if(!$passwordSalt) {
+	$conn->close();
+    die("Invalid credentials.");
+}
+
+$passwordWithSalt = $passwordSalt . $_POST['password'];
+$passwordHash = hash('sha256', $passwordWithSalt);
+
+
+$stmt = $conn->prepare("SELECT COUNT(*) FROM klant WHERE Gebruikersnaam = ? AND Wachtwoord = ?");
+$stmt->bind_param("ss", $_POST['username'], $passwordWithSalt);
 $stmt->execute();
 
 $result = $stmt->get_result();
@@ -24,30 +41,10 @@ $match = $result->fetch_row()[0];
 $stmt->close();
 
 if(!$match) {
-    die("Username not found");
+	$conn->close();
+    die("Invalid credentials.");
 }
 
-$stmt = $conn->prepare("SELECT COUNT(*) FROM klant WHERE Gebruikersnaam = ? AND Wachtwoord = ?");
-$stmt->bind_param("s", $_POST['username']);
-$stmt->execute();
 
-$result = $stmt->get_result();
-$stmt->close();
-
-if($_POST != $result) {
-    die("Entered password is incorrect");
-}
-
-$sql = "SELECT * FROM `parkeerplaats`";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo $row["Idparkeerplaats"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
 $conn->close();
 ?>
