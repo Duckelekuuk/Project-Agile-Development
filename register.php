e656d6cc91a23c486fa7caca7a1421c63c36052d
bugbot6 <bugbot6@gmail.com>
2019-05-13 16:15:02 +0200

    Email check & kenteken len.

--- register.php
+++ register.php
@@ -9,6 +9,10 @@ if(!isset($_POST['registerFullname']) || !isset($_POST['registerEmail']) || !iss
 	die("Incomplete parameters.");
 }
 
+$kenteken = $_POST('registerNumberplate').replace('-', '').toUpperCase();
+if(strlen($kenteken) != 6){
+	die("Invalid Numberplate.");
+}
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -16,6 +20,11 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+if(!preg_match('/^(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){255,})(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){65,}@)(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22))(?:\.(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-[a-z0-9]+)*\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-[a-z0-9]+)*)|(?:\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\]))$/iD', $_POST['registerEmail'])){
+	die("Invalid email.");
+}
+
 
 $stmt = $conn->prepare("SELECT COUNT(*) FROM klant WHERE Emailadres = ?");
 $stmt->bind_param("s", $_POST['registerEmail']);
@@ -30,7 +39,7 @@ if ($match){
 }
 
 $stmt = $conn->prepare("SELECT COUNT(*) FROM klant WHERE Kenteken = ?");
-$stmt->bind_param("s", $_POST['registerNumberplate']);
+$stmt->bind_param("s", $kenteken);
 $stmt->execute();
 
 $result = $stmt->get_result();
@@ -57,7 +66,7 @@ if(strlen($_POST['registerPassword']) < 8){
 	die("Password too short.");
 }
 
-//email check
+
 
 //insert data
 ?>
