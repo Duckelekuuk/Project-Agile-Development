ee77ef67e54a05ebd4ea2f8f3062d230e29014d3
Joey <joeylubke@hotmail.nl>
2019-05-15 12:11:46 +0200

    Logout button

--- src/afterlogin.php
+++ src/afterlogin.php
@@ -1,4 +1,26 @@
 <?php
 session_start();
+
 echo "Session username is " . $_SESSION["username"];
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<form method="post">
+    <input type="submit" name="logout" id="logout" value="logout">
+</form>
+
+<?php
+
+function logout() {
+    session_destroy();
+    header('location:../index.php');
+}
+
+if(array_key_exists('logout',$_POST)) {
+    logout();
+}
+
 ?>
