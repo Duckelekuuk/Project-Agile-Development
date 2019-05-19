59c5cf14faadd8e2ff36476a16c174dbcc348285
Joey <joeylubke@hotmail.nl>
2019-05-19 20:42:26 +0200

    reachable links and removed unused items

--- Reservering.php
+++ Reservering.php
@@ -37,42 +37,33 @@ $csrf = $_SESSION['csrf'];
 
     <!-- <link rel="stylesheet" type="text/css" href="css/bootstrap-datetimepicker.css"> -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.1/moment.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.43/css/bootstrap-datetimepicker.min.css">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.43/css/bootstrap-datetimepicker-standalone.css">
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.43/js/bootstrap-datetimepicker.min.js"></script>
-
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <meta name="description" content="Landing PAGE Html5 Template">
-
-    <meta name="keywords" content="landing,startup,flat">
-
-    <meta name="author" content="Made By GN DESIGNS">
+    <link rel="stylesheet" type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.43/css/bootstrap-datetimepicker.min.css">
+    <link rel="stylesheet" type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.43/css/bootstrap-datetimepicker-standalone.css">
+    <script type="text/javascript"
+            src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.43/js/bootstrap-datetimepicker.min.js"></script>
 
     <link href="assets/css/main.css" rel="stylesheet">
     <title>Reserveren</title>
 
     <!-- // PLUGINS (css files) // -->
 
-    <link href="../assets/js/plugins/bootsnav_files/skins/color.css" rel="stylesheet">
+    <link href="assets/js/plugins/bootsnav_files/skins/color.css" rel="stylesheet">
 
-    <link href="../assets/js/plugins/bootsnav_files/css/animate.css" rel="stylesheet">
+    <link href="assets/js/plugins/bootsnav_files/css/animate.css" rel="stylesheet">
 
-    <link href="../assets/js/plugins/bootsnav_files/css/bootsnav.css" rel="stylesheet">
+    <link href="assets/js/plugins/bootsnav_files/css/bootsnav.css" rel="stylesheet">
 
-    <link href="../assets/js/plugins/bootsnav_files/css/overwrite.css" rel="stylesheet">
+    <link href="assets/js/plugins/bootsnav_files/css/overwrite.css" rel="stylesheet">
 
-    <link href="../assets/js/plugins/owl-carousel/owl.carousel.css" rel="stylesheet">
+    <link href="assets/js/plugins/owl-carousel/owl.carousel.css" rel="stylesheet">
 
-    <link href="../assets/js/plugins/owl-carousel/owl.theme.css" rel="stylesheet">
+    <link href="assets/js/plugins/owl-carousel/owl.theme.css" rel="stylesheet">
 
-    <link href="../assets/js/plugins/owl-carousel/owl.transitions.css" rel="stylesheet">
+    <link href="assets/js/plugins/owl-carousel/owl.transitions.css" rel="stylesheet">
 
-    <link href="../assets/js/plugins/Magnific-Popup-master/Magnific-Popup-master/dist/magnific-popup.css" rel="stylesheet">
+    <link href="assets/js/plugins/Magnific-Popup-master/Magnific-Popup-master/dist/magnific-popup.css" rel="stylesheet">
 
     <!--// ICONS //-->
 
@@ -120,7 +111,7 @@ $csrf = $_SESSION['csrf'];
 
             <a class="navbar-brand" href="#brand">
 
-                <img src="../assets/img/logo.png" class="logo" alt="logo">
+                <img src="assets/img/logo.png" class="logo" alt="logo">
 
             </a>
 
@@ -187,9 +178,9 @@ $csrf = $_SESSION['csrf'];
 
                 </h2>
 
-                <p><b>Heeft u problemen met een parkeerplek vinden waar u ook kunt opladen? Weer rondjes rijden tot er een plek vrij is? Ver geparkeerd staan omdat alle oplaad plekken bezet zijn? Vanaf nu niet meer met eParking!</b></p>
-
-                </a>
+                <p><b>Heeft u problemen met een parkeerplek vinden waar u ook kunt opladen? Weer rondjes rijden tot er
+                        een plek vrij is? Ver geparkeerd staan omdat alle oplaad plekken bezet zijn? Vanaf nu niet meer
+                        met eParking!</b></p>
 
                 <div class="centered" id="centered" style="align-content: center">
                     <form class="datum-tijd" action="/Reserveren.php" method="POST">
@@ -199,7 +190,7 @@ $csrf = $_SESSION['csrf'];
                             <h2>van:</h2>
                             <div class="form-group">
                                 <div class='input-group date' id='datetimepicker6'>
-                                    <input name="Van" type='text' class="form-control" placeholder="Van" />
+                                    <input name="Van" type='text' class="form-control" placeholder="Van"/>
                                     <span class="input-group-addon">
                     <span class="glyphicon glyphicon-calendar"></span>
                 </span>
@@ -247,212 +238,12 @@ $csrf = $_SESSION['csrf'];
 
 </section>
 
-<!--========================================
-
-       About Us
-
-========================================-->
-
-
-
-<section id="about" class="section-padding">
-
-    <div class="container">
-
-        <h2>Over ons</h2>
-
-        <p>Wij zijn een fris bedrijf wat belang heeft voor een makkelijke toekomst.</p>
-
-        <div class="row">
-
-            <div class="col-md-4">
-
-                <div class="icon-box">
-
-                    <i class="material-icons">favorite</i>
-
-                    <h4>Simple To Use</h4>
-
-                    <p>Altijd een parkeerplek zolang je maar op tijd reserveert. Verder hoef je nergens over na te denken.</p>
-
-                </div>
-
-            </div>
-
-            <div class="col-md-4">
-
-                <div class="icon-box">
-
-                    <i class="material-icons">flash_on</i>
-
-                    <h4>Powerful</h4>
-
-                    <p>Simpel in gebruik! Maak een account en reserveren maar!</p>
-
-                </div>
-
-            </div>
-
-            <div class="col-md-4">
-
-                <div class="icon-box">
-
-                    <i class="material-icons">settings</i>
-
-                    <h4>Easy To Customize</h4>
-
-                    <p>Deze app is toegankelijk voor iedereen! Zo hoef je je geen zorgen meer te maken om die ene oplaadpaal bij je werk, als je hem hebt gereserveerd is hij voor jou.</p>
-
-                </div>
-
-            </div>
-
-        </div>
-
-    </div>
-
-</section>
-
-<!--========================================
-
-          Story
-
-   ========================================-->
-
-
-
-<section id="story">
-
-    <div class="container-fluid">
-
-        <div class="row">
-
-            <!-- Img -->
-
-            <div class="col-md-6 story-bg">
-
-            </div>
-
-            <!-- Story Caption -->
-
-            <div class="col-md-6">
-
-                <div class="story-content">
-
-                    <h2>Ons success verhaal</h2>
-
-                    <p class="story-quote">
-
-                        "Success is het vinden van blijdschap in de kleine dingen, tot het punt waar je er niet meer over na hoeft te denken."
-
-                    </p>
-
-                    <div class="story-text">
-
-                        <p>Sinds begin 2019 hervormen en herdefiniëren wij elektrisch parkeren in Amsterdam, en zorgen wij ervoor dat parkeren nog nooit zo makkelijk is geweest.</p>
-
-                    </div>
-
-                    <a href="#" class="btn btn-white">Lees meer</a>
-
-                </div>
-
-            </div>
-
-        </div>
-
-    </div>
-
-</section>
-
-
-
-<!--========================================
-
-       Services
-
-========================================-->
-
-
-
-<section id="services" class="section-padding">
-
-    <div class="container">
-
-        <h2>Onze services</h2>
-
-        <p>Als klant wil je natuurlijk wel dat je bij een fijn bedrijf reserveert, daarvoor hebben wij de volgende services.</p>
-
-        <div class="row">
-
-            <div class="col-md-3">
-
-                <div class="icon-box">
-
-                    <i class="material-icons">thumb_up</i>
-
-                    <h4>Great Quality</h4>
-
-                    <p>Quality ipsum dolor sit amet, consectetur adipisicing elit. Beatae quod error quis.</p>
-
-                </div>
-
-            </div>
-
-            <div class="col-md-3">
-
-                <div class="icon-box">
-
-                    <i class="material-icons">euro_symbol</i>
-
-                    <h4>Best Price</h4>
-
-                    <p>Price ipsum dolor sit amet, consectetur adipisicing elit. Beatae quod error quis.</p>
-
-                </div>
-
-            </div>
-
-            <div class="col-md-3">
-
-                <div class="icon-box">
-
-                    <i class="material-icons">forum</i>
-
-                    <h4>24/7 Support</h4>
-
-                    <p>Support ipsum dolor sit amet, consectetur adipisicing elit. Beatae quod error quis.</p>
-
-                </div>
-
-            </div>
-
-            <div class="col-md-3">
-
-                <div class="icon-box">
-
-                    <i class="material-icons">view_carousel</i>
-
-                    <h4>UX/UI Design</h4>
-
-                    <p>Quality ipsum dolor sit amet, consectetur adipisicing elit. Beatae quod error quis.</p>
-
-                </div>
-
-            </div>
-
-        </div>
-
-    </div>
-
-</section>
 
 <!--========================================
 
        Footer
 
 ========================================-->
-
 
 
 <footer>
@@ -490,22 +281,17 @@ $csrf = $_SESSION['csrf'];
 </footer>
 
 
+<script src="assets/bootstrap-3.3.7/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
 
-<script src="../assets/bootstrap-3.3.7/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+<script src="assets/js/plugins/owl-carousel/owl.carousel.min.js"></script>
 
-<script src="../assets/js/plugins/owl-carousel/owl.carousel.min.js"></script>
+<script src="assets/js/plugins/bootsnav_files/js/bootsnav.js"></script>
 
-<script src="../assets/js/plugins/bootsnav_files/js/bootsnav.js"></script>
+<script src="assets/js/plugins/typed.js-master/typed.js-master/dist/typed.min.js"></script>
 
-<script src="../assets/js/plugins/typed.js-master/typed.js-master/dist/typed.min.js"></script>
+<script src="assets/js/plugins/Magnific-Popup-master/Magnific-Popup-master/dist/jquery.magnific-popup.js"></script>
 
-<script src="../assets/js/plugins/Magnific-Popup-master/Magnific-Popup-master/dist/jquery.magnific-popup.js"></script>
-
-<script src="../assets/js/main.js"></script>
-
-<script src="../assets/js/login.js"></script>
-
-<script src="../assets/js/registratie.js"></script>
+<script src="assets/js/main.js"></script>
 
 </body>
 
