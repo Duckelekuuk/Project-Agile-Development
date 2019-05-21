d5993c5a9205f9d5cb50b455bbe9493bfa363e23
eric.meijlink <eric.meijlink@me.com>
2019-05-21 20:33:36 +0200

    loginhome layout update

--- loginhome.php
+++ loginhome.php
@@ -14,7 +14,7 @@
     <meta name="author" content="Made By GN DESIGNS">
 
 
-    <title>eParking</title>
+    <title>eParking Home</title>
 
 
     <!-- // PLUGINS (css files) // -->
@@ -98,13 +98,20 @@
 
             <ul class="nav navbar-nav navbar-right">
 
-                <li class="active scroll"><a href="#home">Home</a></li>
+                <li class="active scroll"><a href="#home">Reserveren</a></li>
 
-                <li class="scroll"><a href="contact&faq.html">contact & faq</a></li>
+                <li class="scroll"><a href="#about">About</a></li>
+
+                <li class="scroll"><a href="#services">Services</a></li>
+
+                <li class="scroll"><a href="#contact">Contact</a></li>
+
+                <li class="scroll"><a href="faq.html">Faq</a></li>
 
                 <li class="button-holder">
 
-                    <button type="button" class="btn btn-blue navbar-btn" data-toggle="modal" data-target="#Logout">Log out
+                    <button type="button" class="btn btn-blue navbar-btn" data-toggle="modal" data-target="#SignIn">Sign
+                        out
                     </button>
 
                 </li>
@@ -133,11 +140,11 @@
 <script src="assets/js/plugins/Magnific-Popup-master/Magnific-Popup-master/dist/jquery.magnific-popup.js"></script>
 
 <script src="assets/js/main.js"></script>
-<section id="faq" class="section-padding">
+<section id="home" class="section-padding">
     <div class="row">
         <div class="container-fluid">
             <div class="col-md-78">
-                <div class="story-content" style="color: #44c5ee; padding-left: 150px; padding-top: 50px;">
+                <div class="story-content" style="color: #000000; padding-left: 150px; padding-top: 50px;">
                     <h2>Welkom bij eParking, kies de gewenste locatie en reserveer uw oplaadpunt</h2>
                     <br>
                     <div class="card bg-warning " style="color: #000; max-width: 49rem; padding-left: 20px; padding-top: 20px; padding-bottom: 20px; border-radius: 25px">
@@ -155,4 +162,63 @@
     </div>
 </section>
 </body>
+</html>
+
+
+<!--========================================
+
+       Footer
+
+========================================-->
+
+
+<footer>
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="footer-caption">
+
+                <img src="assets/img/logo.png" class="img-responsive center-block" alt="logo">
+
+                <hr>
+
+                <h5 class="pull-left">eParking, &copy;2019 All rights reserved</h5>
+
+                <ul class="liste-unstyled pull-right">
+
+                    <li><a href="#facebook"><i class="fa fa-facebook"></i></a></li>
+
+                    <li><a href="#twitter"><i class="fa fa-twitter"></i></a></li>
+
+                    <li><a href="#linkedin"><i class="fa fa-linkedin"></i></a></li>
+
+                    <li><a href="#instagram"><i class="fa fa-instagram"></i></a></li>
+
+                </ul>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</footer>
+
+
+<script src="assets/bootstrap-3.3.7/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+
+<script src="assets/js/plugins/owl-carousel/owl.carousel.min.js"></script>
+
+<script src="assets/js/plugins/bootsnav_files/js/bootsnav.js"></script>
+
+<script src="assets/js/plugins/typed.js-master/typed.js-master/dist/typed.min.js"></script>
+
+<script src="assets/js/plugins/Magnific-Popup-master/Magnific-Popup-master/dist/jquery.magnific-popup.js"></script>
+
+<script src="assets/js/main.js"></script>
+
+</body>
+
 </html>
