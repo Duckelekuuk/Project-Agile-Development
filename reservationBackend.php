0e8ab592ac42d132820c52e6da0ad63e9895fbef
bugbot6 <bugbot6@gmail.com>
2019-05-21 22:11:39 +0200

    Implemented reservation code.

--- reservationBackend.php
+++ reservationBackend.php
@@ -1,7 +1,5 @@
 <?php
-echo $_POST['csrf'],"\r\n";
-echo $_POST['Van'],"\r\n ";
-echo $_POST['Tot'];
+session_start();
 
 $servername = "localhost:3307";
 $username = "root";
@@ -9,35 +7,45 @@ $password = "usbw";
 $dbname = "mol";
 
 
-if(isset($_POST['submit']) && ($_POST['Van']) && !isset($_POST['Tot']) && !isset($_POST['csrf'])) {
-    die("Incomplete parameters");
-}
-//validate token
-    if($_POST['csrf'] != $_SESSION['csrf']) {
-        die("Invalid CSRF token.");
-    }
+//if(isset($_POST['submit']) && ($_POST['Van']) && !isset($_POST['Tot']) && !isset($_POST['csrf'])) {
+//    die("Incomplete parameters");
+//}
+///validate token
+//f($_POST['csrf'] != $_SESSION['csrf']) {
+//    die("Invalid CSRF token.");
+//}
+
 
 // Create connection
-    $db = new mysqli($servername, $username, $password, $dbname);
-
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-    if ($db->connect_error) {
-        die("Connection failed: " . $db->connect_error);
-    }
+if ($conn->connect_error) {
+    $conn->close();
+    die("Connection failed: " . $conn->connect_error);
+}
 
+$van = date_create_from_format('d-m-Y H:i', $_POST['Van'], new DateTimeZone('CET'))->format('Y-m-d H:00:00');
+$tot = date_create_from_format('d-m-Y H:i', $_POST['Tot'], new DateTimeZone('CET'))->format('Y-m-d H:00:00');
+
+$kenteken = $_SESSION['kenteken'];
+
+$stmt = $conn->prepare("SELECT Idparkeerplaats FROM parkeerplaats WHERE Idparkeerplaats NOT IN (SELECT Idparkeerplaats FROM reserveringen WHERE ? < Tot AND ? > Van)");
+$stmt->bind_param("ss", $van, $tot);
+$stmt->execute();
+
+$result = $stmt->get_result();
+$plaats = $result->fetch_row()[0];
+$stmt->close();
+
+if(empty($plaats)) {
+    $conn->close();
+    die("No spot availible during this time-frame.");
+}
 
 // Get fields value
-    $Van = $db->real_escape_string($_POST['Van']);
-    $Tot = $db->real_escape_string($_POST['Tot']);
+    $stmt = $conn->prepare("INSERT INTO reserveringen (Kenteken, Idparkeerplaats, Van, Tot) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $kenteken, $plaats, $van, $tot);
+    $stmt->execute();
+    $stmt->close();
 
-    // Insert datetime into the database
-    $insert = $db->query("INSERT INTO reserveringen (Van, Tot) VALUES ('" . $Van . "', '" . $Tot . "')");
-
-    // If insert successful
-    if ($insert) {
-        $status = 'Event data inserted successfully. Event ID: ' . $db->insert_id;
-    } else {
-        $status = 'Failed to insert event data.';
-
-
-}
+echo("Reserved.");
